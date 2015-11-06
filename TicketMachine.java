e6f799a4ac975cdbc779571052d194de489ce538
David <rulateunmai@hotmail.com>
2015-11-06 13:24:59 +0100

    Añadido metodo para vaciar la maquina

--- TicketMachine.java
+++ TicketMachine.java
@@ -99,4 +99,12 @@ public class TicketMachine
         balance = 0;
         return amountToRefund;
     }
+    
+    public int emptyMachine()
+    {
+        int vaciaMaquina;
+        vaciaMaquina = total;
+        total = 0;
+        return vaciaMaquina;
+    }
 }
